88a2a9040935bd8816056a62cbf1018d4fddca6a
TongyuZhao666 <zhtongyu915@126.com>
2026-02-10 21:07:00 +0900

    final working version

--- sql/create.sql
+++ sql/create.sql
@@ -4,9 +4,5 @@ CREATE TABLE IF NOT EXISTS recipes (
   making_time TEXT,
   serves TEXT,
   ingredients TEXT,
-  cost INTEGER
+  cost TEXT
 );
-
-INSERT INTO recipes (title, making_time, serves, ingredients, cost) VALUES
-('チキンカレー', '45分', '4人', '玉ねぎ,肉,スパイス', 1000),
-('オムライス', '30分', '2人', '玉ねぎ,卵,スパイス,醤油', 700);
